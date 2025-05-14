f3e33cac2a6dd8a4b8ab8abb13262faeb2e8009c
lwilliams002 <cmario72.lw@gmail.com>
2025-05-14 09:51:42 -0700

    Adjusted everything for easy fixes in the future

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import {BrowserRouter} from "react-router-dom";
 import { ChakraProvider} from "@chakra-ui/react";
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
+  <BrowserRouter basename={"/Oil-CRM/"}>
       <ChakraProvider>
           <App />
       </ChakraProvider>
