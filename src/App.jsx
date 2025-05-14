8a6c536df1e96db492a1dcb2758167933a83d6a9
lwilliams002 <cmario72.lw@gmail.com>
2025-05-14 09:34:39 -0700

    Adjusted everything for easy fixes in the future

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Routes, Route, Navigate } from 'react-router-dom'
+import { Routes, Route, Navigate, Outlet } from 'react-router-dom'
 import Layout from './components/Layout.jsx'
 import Dashboard from './pages/Dashboard'
 import NewPatient from './pages/NewPatient'
@@ -15,26 +15,31 @@ function App() {
   return (
     <Routes>
       <Route path="/login" element={<Login />} />
+      {/* Protected routes */}
       <Route
-        path="/*"
+        path="/"
         element={
-        <ProtectedRoute>
-          <Layout>
-            <Routes>
-              <Route path="/" element= {<Dashboard />}/>
-              <Route path="/dashboard" element={<Dashboard />} />
-              <Route path="/patients/new" element={<NewPatient />} />
-              <Route path="/patients" element={<AllPatients />} />
-              <Route path="/appointments/new" element={<NewAppointment />} />
-              <Route path="/patient-details/:id" element={<PatientDetails />} />
-              <Route path="/consent/:id" element={<Consent />} />
-              <Route path="/terms/:id" element={<Terms />} />
-              {/* Add more routes here as needed */}
-            </Routes>
-          </Layout>
-        </ProtectedRoute>
+          <ProtectedRoute>
+            <Layout>
+              {/* Rendered by nested routes */}
+              <Outlet />
+            </Layout>
+          </ProtectedRoute>
         }
-      />
+      >
+        {/* Dashboard at root */}
+        <Route index element={<Dashboard />} />
+        <Route path="dashboard" element={<Dashboard />} />
+        <Route path="patients/new" element={<NewPatient />} />
+        <Route path="patients" element={<AllPatients />} />
+        <Route path="appointments/new" element={<NewAppointment />} />
+        <Route path="patient-details/:id" element={<PatientDetails />} />
+        <Route path="consent/:id" element={<Consent />} />
+        <Route path="terms/:id" element={<Terms />} />
+        {/* Add more nested routes here */}
+      </Route>
+      {/* Fallback for unknown paths */}
+      <Route path="*" element={<Navigate to="/" replace />} />
     </Routes>
   )
 }
