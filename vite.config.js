836b536bf9dc65d52064e87b12844ac3c433478b
lwilliams002 <cmario72.lw@gmail.com>
2025-05-14 08:48:44 -0700

    Adjusted everything for easy fixes in the future

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/Oil-CRM/',    // ← this must match your GitHub Pages repo name
   plugins: [react()],
 })
